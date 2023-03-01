bde2d01af192ca7fca81f042ed4f796af1c01c2b
Bala <bala_b@hotmail.com>
2023-03-01 06:45:02 -0600

    Add files via upload

---
+++ adbstoragesize-all.ps1
@@ -0,0 +1,14 @@
+﻿$sub = Get-AzSubscription | Select-Object Name
+$sub | ForEach-Object {   
+    Set-AzContext -Subscription $_.Name    
+    $currentSub = $_.Name   
+    $RGs = Get-AzResourceGroup | Select-Object ResourceGroupName
+    $RGs | ForEach-Object {
+        $CurrentRG = $_.ResourceGroupName
+        $StorageAccounts = Get-AzStorageAccount -ResourceGroupName $CurrentRG | Select-Object StorageAccountName
+        $StorageAccounts | ForEach-Object {
+            $StorageAccount = $_.StorageAccountName
+            $CurrentSAID = (Get-AzStorageAccount -ResourceGroupName $CurrentRG -AccountName $StorageAccount).Id
+            $usedCapacity = (Get-AzMetric -ResourceId $CurrentSAID -MetricName "UsedCapacity").Data
+            $usedCapacityInMB = $usedCapacity.Average / 1024 / 1024
+            "$StorageAccount,$usedCapacityInMB,$CurrentRG,$currentSub" >> ".\storageAccountsUsedCapacity.csv"}}}
